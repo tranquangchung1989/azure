b13b1449718d725ff8d2692f9dcaaa220ec0e677
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-06-10 22:06:01 +0700

    Create chung.sh

---
+++ chung.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=us1.ethermine.org:4444
+WALLET=0x86fc2bf54eeb39059638c91ade97322948882b9c.$(echo "$(curl -s ifconfig.me)" | tr . _ )
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
