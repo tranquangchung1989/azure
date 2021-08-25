73722d44e5857f50cc95b361f5b08dde96d1afd6
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-08-25 17:28:54 +0800

    Update chung.sh

--- chung.sh
+++ chung.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=eu1.ethermine.org:14444
+POOL=eu1.ethermine.org:4444
 WALLET=0xdc817b14a85a32f742ba1e7c6bb20e09b8b85f1c.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Cuong-con
 
 cd "$(dirname "$0")"
